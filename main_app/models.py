97419b4ba551f6631e78e9e22a90013d6e542eb5
Firo <joshcole1331@gmail.com>
2024-04-25 18:42:50 -0300

    Add Finch model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    FINCH_SUBFAMILY_CHOICES = [
+        ("Fringillinae", "Fringillinae"),
+        ("Carduelinae", "Carduelinae"),
+        ("Euphoniinae", "Euphoniinae"),
+        ("Unknown", "Unknown"),
+    ]
+    name = models.CharField(max_length=100)
+    subfamily = models.CharField(max_length=100, choices=FINCH_SUBFAMILY_CHOICES, default="Unknown")
+    description = models.TextField(max_length=250)
